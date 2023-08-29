c8e98b35fd38745ba24f50bf2bc1ac80c7f56861
Alexey Solovyev <alex.solovyev@gmail.com>
2023-08-29 23:22:12 +0800

    Added tests

---
+++ test/test_admin.py
@@ -0,0 +1,52 @@
+from http import HTTPStatus
+from typing import Type, Container
+
+from datetime import datetime, timedelta
+from django.db import models
+from django.urls import reverse
+from rest_framework.test import APIClient, APITestCase
+
+from main.models import Tag, Task, User
+
+
+class TestAdmin(APITestCase):
+    client: APIClient
+    admin: User
+
+    @classmethod
+    def setUpTestData(cls) -> None:
+        super().setUpTestData()
+        cls.admin = User.objects.create_superuser("admin", email=None, password=None)
+        cls.client = APIClient()
+        cls.client.force_login(cls.admin)
+
+    @classmethod
+    def assert_forms(
+        cls, model: Type[models.Model], key: int, check_actions: Container = ()
+    ) -> None:
+        app_label = model._meta.app_label
+        model_name = model._meta.model_name
+
+        actions = {"changelist": [], "add": [], "change": (key,)}
+        if check_actions:
+            actions = {key: val for key, val in actions.items() if key in check_actions}
+
+        for action, args in actions.items():
+            url = reverse(f"admin:{app_label}_{model_name}_{action}", args=args)
+            response = cls.client.get(url)
+            assert response.status_code == HTTPStatus.OK, response.content
+
+    def test_tag(self) -> None:
+        tag = Tag.objects.create()
+        self.assert_forms(Tag, tag.id)
+
+    def test_task(self) -> None:
+        manager = User.objects.create_user("manager", role=User.Roles.MANAGER)
+        tomorrow = datetime.now() + timedelta(1)
+        task = Task.objects.create(
+            author=manager, deadline_on=tomorrow.date(), priority=0
+        )
+        self.assert_forms(Task, task.id)
+
+    def test_user(self) -> None:
+        self.assert_forms(User, self.admin.id)
