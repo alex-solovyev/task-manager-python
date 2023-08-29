dc6c9d88f59a15f66e558547520772a7866408e2
Alexey Solovyev <alex.solovyev@gmail.com>
2023-08-29 11:47:47 +0800

    Customize the admin site

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import Tag, Task, User
 
-admin.site.register(Tag)
-admin.site.register(Task)
-admin.site.register(User, UserAdmin)
+
+class TaskManagerAdminSite(admin.AdminSite):
+    pass
+
+
+task_manager_admin_site = TaskManagerAdminSite(name="Task manager admin")
+
+
+@admin.register(Tag, site=task_manager_admin_site)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ["id", "title"]
+
+
+@admin.register(Task, site=task_manager_admin_site)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ["id", "title"]
+
+
+@admin.register(User, site=task_manager_admin_site)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("role",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("role",)}),)
